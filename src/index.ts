7a13f8bc2fe283e1a9969add8d2538a131990f67
James Monger <jkmonger96@gmail.com>
2018-06-26 23:26:00 +0100

    Create function outlines

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import * as Long from "long";
+
+const hash = (input: string) => {
+    return Long.fromInt(0);
+};
+
+const unhash = (input: Long) => {
+    return "";
+};
+
+export {
+    hash,
+    unhash
+};
